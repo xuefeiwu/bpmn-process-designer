c3f4b3ce86833faa01e71098d335e4ff96467e4a
yangyi <yangyi@51qqt.com>
2023-02-18 22:03:26 +0800

    add: 流程初始化显示开始节点

--- utils/xml.js
+++ utils/xml.js
@@ -1,20 +1,21 @@
 import { catchError, catchWarning } from './printCatch'
 
 export function emptyXML (key, name) {
-    return `<?xml version="1.0" encoding="UTF-8"?>
-<bpmn:definitions 
-  xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-  xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL"
-  xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI"
-  xmlns:dc="http://www.omg.org/spec/DD/20100524/DC"
-  xmlns:di="http://www.omg.org/spec/DD/20100524/DI"
-  targetNamespace="http://bpmn.io/schema/bpmn"
-  id="Definitions_0001">
-  <bpmn:process id="${key}" name="${name}" isExecutable="true" />
+    return `
+<?xml version="1.0" encoding="utf-8"?>
+<bpmn:Definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" id="Definitions_0001" targetNamespace="http://bpmn.io/schema/bpmn">
+  <bpmn:process id="${key}" name="${name}" isExecutable="true">
+    <bpmn:startEvent id="StartEvent_1" name="开始"/>
+  </bpmn:process>
   <bpmndi:BPMNDiagram id="BPMNDiagram_1">
-    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="${key}" />
+    <bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="${key}">
+      <bpmndi:BPMNShape id="StartEvent_1_di" bpmnElement="StartEvent_1">
+        <dc:Bounds x="292" y="212" width="36" height="36"/>
+      </bpmndi:BPMNShape>
+    </bpmndi:BPMNPlane>
   </bpmndi:BPMNDiagram>
-</bpmn:definitions>`
+</bpmn:Definitions>
+`
 }
 
 export async function createNewDiagram (modeler, newXml, settings) {
