5275278e5d0dd9bb9b69309700b6951d7a94b49b
yangyi <yangyi@51qqt.com>
2023-02-23 20:57:54 +0800

    add: 创建节点属性

--- packages/bpmn-utils/BpmnFactoryUtils.js
+++ packages/bpmn-utils/BpmnFactoryUtils.js
@@ -3,6 +3,6 @@ import {getModeler} from '@packages/bpmn-utils/BpmnDesignerUtils'
 export function createFactoyElement (elementType, properties, parent) {
     let bpmnFactory = getModeler.get('bpmnFactory')
     let element = bpmnFactory.create(elementType, properties)
-    element.$parent = parent
+    parent && (element.$parent = parent)
     return element
 }
